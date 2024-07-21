42c502ea70da3a247d74b0e23bcf6c2f401b031e
Davyjakim <jakimdavy07@gmail.com>
2024-07-21 12:05:44 +0200

    checking db

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
-const config = require('config')
+const config = require("config");
 
-module.exports= function(){
-  const db = process.env.db_url
-    mongoose
-  .connect(db)
-  .then(() => {
-    console.log(`Connected to ${db}...`);
-  })
-}
+module.exports = function () {
+  const db = process.env.db_url;
+  mongoose
+    .connect(db)
+    .then(() => {
+      console.log(`Connected to ${db}...`);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
